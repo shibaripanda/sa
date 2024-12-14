5f9dfa5bf178cef0e00616f0104a747aaf73184b
Dima <prevetlunatikam@gmail.com>
2024-12-14 21:40:24 +0300

    first commit

---
+++ server/src/main.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 5002
+  const app = await NestFactory.create(AppModule)
+  // global.connectUsers = {}
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  })
+
+  await app.listen(PORT, () => {console.log(`Server SERVICEAPP started on port = ${PORT}`)})
+}
+bootstrap()
